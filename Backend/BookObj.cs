1b5157ba459bfa92eb38d36ff6d90b92d7206f96
Kye <promasterboy@outlook.com>
2022-08-22 18:56:44 +1000

    cover and book spine fix?

--- Backend/BookObj.cs
+++ Backend/BookObj.cs
@@ -85,7 +85,12 @@ namespace EveryoneReads.Backend
             }
             else
             {
+
+#if DEBUG
                 newBook.CoverURL = "/NoCover.png";
+#else
+                newBook.CoverURL = "Everyone-Reads/NoCover.png";
+#endif
             }
 
             if (book.volumeInfo.publishedDate != null)
